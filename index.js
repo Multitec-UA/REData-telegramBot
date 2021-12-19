7aec5ffd8bc44de43e88b66f3e0483605fcef5c9
Esteban Escobar <juantaban@gmail.com>
2021-12-19 23:57:34 +0100

    added telegram bot api and created getcurrentprice command

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
+const TelegramBot = require('node-telegram-bot-api');
 const got = require('got');
 require('dotenv').config();
 
+// Telegram bot token
+const token = '5019808755:AAFhYK9nFJz_K_uSyppIjHZJMhbW75zh_4s';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+/**
+ * Controls /currentPrice bot command.
+ */
+bot.onText(/\/currentPrice/, async (msg) => {
+    const chatId = msg.chat.id;
+    const price = await getCurrentPrice();
+
+    const today = new Date();
+    const time = today.getHours() + ":" + today.getMinutes();
+    
+    bot.sendMessage(chatId, "El precio de la luz a las " + time + " es de " + price + "€ por MWh.");
+  });
+
+/**
+ * Gets the current spot market price in €/MWh
+ * @returns string finalPrice
+ */
 const getCurrentPrice = async () => {
     const startDate = new Date().setMinutes(0);
     const endDate = new Date().setMinutes(59);
